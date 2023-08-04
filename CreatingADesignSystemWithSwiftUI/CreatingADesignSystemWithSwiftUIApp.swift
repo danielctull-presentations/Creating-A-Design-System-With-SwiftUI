5301109f5501f4ebffaf6b3283480b5423b12962
Daniel Tull <dt@danieltull.co.uk>
2023-08-04 08:38:39 +0100

    Initial Commit

---
+++ CreatingADesignSystemWithSwiftUI/CreatingADesignSystemWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingADesignSystemWithSwiftUIApp.swift
+//  CreatingADesignSystemWithSwiftUI
+//
+//  Created by Daniel Tull on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CreatingADesignSystemWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
