e7523ccf5fa7e93c94a79d56aee600ca44e1c1cc
Daniel Tull <dt@danieltull.co.uk>
2023-09-05 09:35:15 +0100

    Add bullet points for design systems

--- CreatingADesignSystemWithSwiftUI/1.WhatIsADesignSystem.swift
+++ CreatingADesignSystemWithSwiftUI/1.WhatIsADesignSystem.swift
@@ -6,7 +6,14 @@ struct WhatIsADesignSystem: View {
 
     var body: some View {
         Group {
-            Slide("What is a Design System?")
+            Slide("What is a Design System?") {
+                BulletedList {
+                    Bullet("Process")
+                    Bullet("Documentation")
+                    Bullet("Common resources")
+                    Bullet("Functional libraries")
+                }
+            }
         }
     }
 }
