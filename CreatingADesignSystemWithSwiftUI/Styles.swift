c990fc46bdaa28db2c181133b1bb4bd696a6caeb
Daniel Tull <dt@danieltull.co.uk>
2024-01-26 17:25:48 +0000

    Use custom initialiser for ViewThatFits

--- CreatingADesignSystemWithSwiftUI/Styles.swift
+++ CreatingADesignSystemWithSwiftUI/Styles.swift
@@ -101,11 +101,45 @@ struct FittingCodeStyle: CodeStyle {
     fileprivate let weight: Font.Weight
 
     func makeBody(configuration: Configuration) -> some View {
-        
-        configuration.fitting(min: 3, max: idealSize) { size in
+        ViewThatFits(range: 3...max(4, idealSize)) { size in
             configuration.code
                 .lineSpacing(size / 8)
                 .font(.system(size: size, weight: weight, design: .monospaced))
+        }
+    }
+}
+
+extension ViewThatFits {
+    
+    /// Provides a variant of ViewThatFits that fits the content of the largest
+    /// size in the given range.
+    ///
+    /// - Parameters:
+    ///   - range: The range of sizes to evaluate.
+    ///   - content: Content for the given size.
+    public init<C: View, Size: BinaryFloatingPoint>(
+        range: ClosedRange<Size>,
+        @ViewBuilder content: @escaping (Size) -> C
+    ) where Content == ReversedRangeView<C, Size> {
+        self.init {
+            ReversedRangeView(range: range, content: content)
+        }
+    }
+}
+
+public struct ReversedRangeView<Content: View, Bound: BinaryFloatingPoint>: View {
+
+    private let range: ClosedRange<Int>
+    private let content: (Bound) -> Content
+
+    init(range: ClosedRange<Bound>, content: @escaping (Bound) -> Content) {
+        self.range = (Int(ceil(range.lowerBound))...Int(floor(range.upperBound)))
+        self.content = content
+    }
+
+    public var body: some View {
+        ForEach(range.reversed(), id: \.self) { size in
+            content(Bound(size))
         }
     }
 }
