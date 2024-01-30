6e3c74837cca67a3be6fd2ea1db0f6c6bfc34791
Daniel Tull <dt@danieltull.co.uk>
2024-01-30 08:40:50 +0000

    Use built-in Xcode code highlighting

--- CreatingADesignSystemWithSwiftUI/Styles.swift
+++ CreatingADesignSystemWithSwiftUI/Styles.swift
@@ -1,6 +1,7 @@
 
 import SlideUI
 import SwiftUI
+import XcodeCodeHighlighting
 
 // MARK: - TitleSlideStyle
 
@@ -73,7 +74,7 @@ struct ContentSlideStyle: SlideStyle {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .padding(scale * 18)
             .codeStyle(.fitting(idealSize: scale * 14))
-            .codeHighlighting(.xcodeClassic)
+            .codeHighlighting(.xcodeClassicLight)
         }
     }
 
@@ -140,51 +141,6 @@ public struct ReversedRangeView<Content: View, Bound: BinaryFloatingPoint>: View
     public var body: some View {
         ForEach(range.reversed(), id: \.self) { size in
             content(Bound(size))
-        }
-    }
-}
-
-extension CodeHighlighting where Self == XcodeClassicCodeHighlighting {
-    static var xcodeClassic: Self { Self() }
-}
-
-struct XcodeClassicCodeHighlighting: CodeHighlighting {
-
-    func color(for token: Token) -> Color {
-        switch token.classification {
-        case .attribute: Color(red: 0.505801, green: 0.371396, blue: 0.012096, opacity: 1)
-        case .character: Color(red: 0.11, green: 0, blue: 0.81, opacity: 1)
-        case .comment: Color(red: 0, green: 0.456, blue: 0, opacity: 1)
-        case .commentDoc: Color(red: 0, green: 0.456, blue: 0, opacity: 1)
-        case .commentDocKeyword: Color(red: 0.008, green: 0.239, blue: 0.063, opacity: 1)
-        case .declarationOther: Color(red: 0.0588235, green: 0.407843, blue: 0.627451, opacity: 1)
-        case .declarationType: Color(red: 0.0431373, green: 0.309804, blue: 0.47451, opacity: 1)
-        case .identifierClass: Color(red: 0.109812, green: 0.272761, blue: 0.288691, opacity: 1)
-        case .identifierClassSystem: Color(red: 0.224543, green: 0, blue: 0.628029, opacity: 1)
-        case .identifierConstant: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
-        case .identifierConstantSystem: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
-        case .identifierFunction: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
-        case .identifierFunctionSystem: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
-        case .identifierMacro: Color(red: 0.391471, green: 0.220311, blue: 0.124457, opacity: 1)
-        case .identifierMacroSystem: Color(red: 0.391471, green: 0.220311, blue: 0.124457, opacity: 1)
-        case .identifierType: Color(red: 0.109812, green: 0.272761, blue: 0.288691, opacity: 1)
-        case .identifierTypeSystem: Color(red: 0.224543, green: 0, blue: 0.628029, opacity: 1)
-        case .identifierVariable: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
-        case .identifierVariableSystem: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
-        case .keyword: Color(red: 0.607592, green: 0.137526, blue: 0.576284, opacity: 1)
-        case .mark: Color(red: 0.14902, green: 0.458824, blue: 0.027451, opacity: 1)
-        case .markupCode: Color(red: 0.665, green: 0.052, blue: 0.569, opacity: 1)
-        case .number: Color(red: 0.11, green: 0, blue: 0.81, opacity: 1)
-        case .plain: Color(red: 0, green: 0, blue: 0, opacity: 0.85)
-        case .preprocessor: Color(red: 0.391471, green: 0.220311, blue: 0.124457, opacity: 1)
-        case .regex: Color(red: 0.77, green: 0.102, blue: 0.086, opacity: 1)
-        case .regexCapturename: Color(red: 0.194184, green: 0.429349, blue: 0.454553, opacity: 1)
-        case .regexCharname: Color(red: 0.421903, green: 0.212783, blue: 0.663785, opacity: 1)
-        case .regexNumber: Color(red: 0.11, green: 0, blue: 0.81, opacity: 1)
-        case .regexOther: Color(red: 0, green: 0, blue: 0, opacity: 0.85)
-        case .string: Color(red: 0.77, green: 0.102, blue: 0.086, opacity: 1)
-        case .url: Color(red: 0.055, green: 0.055, blue: 1, opacity: 1)
-        default: Color(red: 0, green: 0, blue: 0, opacity: 0.85)
         }
     }
 }
